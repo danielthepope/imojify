549d27426e416418de71b743a63c80565122edea
Marcus Noble <AverageMarcus@users.noreply.github.com>
2015-11-11 09:30:18 +0000

    Do global replace

--- js/gemojify.js
+++ js/gemojify.js
@@ -2,7 +2,7 @@ function gemojify() {
   var elementsToReplace = document.getElementsByClassName('gemojify');
   for (var i = 0; i < elementsToReplace.length; i++) {
     elementsToReplace[i].innerHTML = elementsToReplace[i].innerHTML
-      .replace(':+1:', ':_1:') // special character not compatible with CSS
+      .replace(/:\+1:(?![^<]*>)/g, ':_1:') // special character not compatible with CSS
       .replace(/:([\w-]+):(?![^<]*>)/g, '<span class="emoji emoji-$1"></span>');
   }
 }
