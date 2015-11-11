d4b611c7a2e7b73b6bdf4b10ddc71c6979929f96
Marcus Noble <marcus@marcusnoble.co.uk>
2015-11-11 06:56:12 +0000

    Pass in scope

--- js/gemojify.js
+++ js/gemojify.js
@@ -1,9 +1,8 @@
-function gemojify() {
-  var elementsToReplace = document.getElementsByClassName('gemojify');
+function gemojify(scope) {
+  scope = scope || '.gemojify';
+  var elementsToReplace = document.querySelectorAll(scope);
   for (var i = 0; i < elementsToReplace.length; i++) {
     elementsToReplace[i].innerHTML = elementsToReplace[i].innerHTML
       .replace(/:([\w-]+):(?![^<]*>)/g, '<span class="emoji emoji-$1"></span>');
   }
 }
-
-gemojify();
