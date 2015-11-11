505ffef86a0034b14b6a27197908dfba1b04d1f3
Daniel Pope <danielthepope@users.noreply.github.com>
2015-11-11 01:01:50 +0000

    Using @benfoxall's super-neat code #1 #2

--- js/gemojify.js
+++ js/gemojify.js
@@ -1,30 +1,8 @@
 function gemojify() {
   var elementsToReplace = document.getElementsByClassName('gemojify');
   for (var i = 0; i < elementsToReplace.length; i++) {
-    gemojify_node(elementsToReplace[i]);
-  }
-}
-
-function gemojify_node(node) {
-  if (node.constructor === Text) {
-    var matches = node.data.match(/:[\w-]+:/g);
-    if (!matches) return;
-    matches.forEach(function (match) {
-      var emojiName = match.replace(/:/g, '');
-      var pre = node.data.substring(0,node.data.indexOf(match));
-      var post = node.data.substring(node.data.indexOf(match) + match.length);
-      var textNode = document.createTextNode(pre);
-      var emojiNode = document.createElement('span');
-      emojiNode.className = "emoji emoji-"+emojiName;
-      node.parentNode.insertBefore(textNode,node);
-      node.parentNode.insertBefore(emojiNode,node);
-      node.data = post;
-    });
-  } else {
-    var childNodes = node.childNodes;
-    for (var i = 0; i < childNodes.length; i++) {
-      gemojify_node(childNodes[i]);
-    }
+    elementsToReplace[i].innerHTML = elementsToReplace[i].innerHTML
+      .replace(/:([\w-]+):(?![^<]*>)/g, '<span class="emoji emoji-$1"></span>');
   }
 }
 
