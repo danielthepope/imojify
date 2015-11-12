00711a2c9ad977a6b1262d8ed1afa664a855e197
Daniel Pope <danielthepope@users.noreply.github.com>
2015-11-12 00:43:55 +0000

    Fixes #8
    Removes the PNG filter so will technically allow any file format. This does mean that animated GIFs are now supported.
    
    Also added some extra background CSS attributes to better support non-square images

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var inline = false;
-var output = 'sass/includes/emoji-' + (inline ? 'inline' : 'url') + '.scss';
+var output = 'sass/includes/emoji.scss';
 var emojiLocation = 'images/emojis';
 
 String.prototype.endsWith = function(suffix) {
@@ -10,16 +10,14 @@ String.prototype.endsWith = function(suffix) {
 fs.writeFileSync(output, '');
 var files = fs.readdirSync(emojiLocation);
 files.forEach(function(element) {
-  if (element.endsWith('.png')) {
-    var name = element.split('.')[0].replace('+','_');
-    var sass = '';
-    if (inline) {
-      var data = fs.readFileSync(emojiLocation + '/' + element);
-      var b64 = new Buffer(data).toString('base64');
-      sass = '.emoji-'+name+' {\n  background-image:url(data:image/png;base64,'+b64+');\n}\n\n';
-    } else {
-      sass = '.emoji-'+name+' {\n  background-image: url("../' + emojiLocation + '/' + element + '");\n}\n\n';
-    }
-    fs.appendFileSync(output, sass);
+  var name = element.split('.')[0].replace(/\+/g,'_');
+  var sass = '';
+  if (inline) {
+    var data = fs.readFileSync(emojiLocation + '/' + element);
+    var b64 = new Buffer(data).toString('base64');
+    sass = '.emoji-'+name+' {\n  background-image:url(data:image/png;base64,'+b64+');\n}\n\n';
+  } else {
+    sass = '.emoji-'+name+' {\n  background-image: url("../' + emojiLocation + '/' + element + '");\n}\n\n';
   }
+  fs.appendFileSync(output, sass);
 });
